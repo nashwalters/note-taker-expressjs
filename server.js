dc183d11733d206c1d29d9cf393670b2a4515a8a
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-02-03 11:59:57 -0500

    Added HTML routes.

--- server.js
+++ server.js
@@ -11,8 +11,17 @@ const PORT = process.env.PORT || 5000;
 //displays static files such as css & js
 app.use(express.static('public')) 
 
+// Get requests
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+// If no matching route is found default to home
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
 //Listener
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
-  });
+});
     
