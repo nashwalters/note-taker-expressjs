fc2c819257ee83057e348e9dc8a3b2f257e1846b
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-02-04 19:47:05 -0500

    Added get api route.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 // Set port
 const PORT = process.env.PORT || 5000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //displays static files such as css & js
 app.use(express.static('public')) 
 
@@ -15,10 +19,15 @@ app.use(express.static('public'))
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
 // If no matching route is found default to home
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+});
 
 //Listener
 app.listen(PORT, function() {
